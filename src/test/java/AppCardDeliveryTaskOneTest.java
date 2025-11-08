a1c417b4f2d71a686fb4699bdcab7d420ceaf178
BRONKS <nazim.said@yandex.ru>
2025-11-09 01:22:49 +0300

    Second Try. Test was rework, filse of project corrected

---
+++ src/test/java/AppCardDeliveryTaskOneTest.java
@@ -0,0 +1,43 @@
+import static com.codeborne.selenide.Selenide.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class AppCardDeliveryTaskOneTest {
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999");
+    }
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now()
+                .plusDays(addDays)
+                .format(DateTimeFormatter.ofPattern(pattern));
+    }
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        $("[data-test-id='city'] input").setValue("Рязань");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[date-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иванов-Иван_Иванович");
+        $("[data-test-id='phone'] input").setValue("+79022090442");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $("notification__content")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно забронирована на " + planningDate));
+    }
+}
